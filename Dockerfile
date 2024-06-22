b7b4ee2311777556bf245a58b41be2a38b7877cd
neJok <uk6bobh@proton.me>
2024-06-22 14:53:17 +0300

    update avatar upload

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,11 @@ ADD . /app
 
 WORKDIR /app
 
+RUN apk update && \
+    apk add --no-cache \
+        libpng-dev \
+        libjpeg-turbo-dev \
+        libwebp-dev
 RUN go build -o main cmd/main.go
 
 CMD ["/app/main"]
--- api/controller/account_controller.go
+++ api/controller/account_controller.go
@@ -11,9 +11,9 @@ import (
 
 	"github.com/neJok/StonTactics/bootstrap"
 	"github.com/neJok/StonTactics/domain"
+	"github.com/nfnt/resize"
 	"github.com/nickalie/go-webpbin"
 	"go.mongodb.org/mongo-driver/bson"
-	"github.com/nfnt/resize"
 
 	"path/filepath"
 
@@ -90,13 +90,15 @@ func (ac *AccountController) Update(c *gin.Context) {
 	}
 
 	newFilename := fmt.Sprintf("%s.webp", userID)
-	savePath := filepath.Join("media/avatars", newFilename)
-
-	if err := os.MkdirAll("media/avatars", os.ModePerm); err != nil {
+	savePath := filepath.Join("/app/media/avatars", newFilename)
+	
+	fmt.Println("create dir")
+	if err := os.MkdirAll("/app/media/avatars", os.ModePerm); err != nil {
 		c.JSON(http.StatusInternalServerError, domain.ErrorResponse{Message: err.Error()})
 		return
 	}
 
+	fmt.Println("remove old avatar")
 	if _, err := os.Stat(savePath); err == nil {
 		if err := os.Remove(savePath); err != nil {
 			c.JSON(http.StatusInternalServerError, domain.ErrorResponse{Message: err.Error()})
@@ -104,6 +106,7 @@ func (ac *AccountController) Update(c *gin.Context) {
 		}
 	}
 
+	fmt.Println("decode img")
 	var img image.Image
 	switch format {
 	case "jpg", "jpeg":
@@ -119,19 +122,24 @@ func (ac *AccountController) Update(c *gin.Context) {
 		return
 	}
 
+	fmt.Println("resize img")
 	resizedImg := resize.Resize(500, 500, img, resize.Lanczos3)
 
+	fmt.Println("create file")
 	f, err := os.Create(savePath)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, domain.ErrorResponse{Message: err.Error()})
 		return
 	}
 
+	fmt.Println("encode img")
 	if err := webpbin.Encode(f, resizedImg); err != nil {
 		f.Close()
 		c.JSON(http.StatusInternalServerError, domain.ErrorResponse{Message: err.Error()})
 		return
 	}
+	
+	fmt.Println("close file")
 
 	if err := f.Close(); err != nil {
 		c.JSON(http.StatusInternalServerError, domain.ErrorResponse{Message: err.Error()})
